4813192bc9accbb75586dc0cda1672881b33dd4b
Mauricio Barg <mrwbarg@gmail.com>
2019-04-23 17:59:13 -0300

    Fix binsearch

--- Algoritmos/BinarySearch.py
+++ Algoritmos/BinarySearch.py
@@ -6,6 +6,8 @@ V = MergeSort([randint(0, 100) for x in range(10)])
 def BinarySearch(i, A):
     n = len(A)
     mid = n // 2
+    if len(A) == 0:
+        return False
     if A[mid] != i and n > 1:
         if A[mid] < i:
             return BinarySearch(i, A[(mid + 1):])
